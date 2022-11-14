74f0eb5ed2bf48a1f4f0fcde9c0dae0111fabb2f
andreaa21 <andrea.zollini@proton.me>
2022-11-14 14:23:06 +0100

    add Component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
-<template>
-  
-</template>
-
 <script>
+
+import Apptitle from './components/Apptitle.vue'
+
+
 export default {
+  name: 'App',
+  components: {
+    Apptitle
+  }
 
 }
 </script>
+
+<template>
+  <Apptitle/>
+</template>
 
 <style>
 
---
+++ src/components/Apptitle.vue
@@ -0,0 +1,25 @@
+
+
+<script>
+export default {
+    name: 'Apptitle'
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>
+            La mia prima app con Vite!
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+    .container{
+        width: 80%;
+        margin: 50px auto;
+    }
+    h1{
+        color:brown;
+    }
+</style>
